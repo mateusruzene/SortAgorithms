3cbee25a30009882e97add12217936486b8c61d6
Mateus Ruzene <mateus.ruzene@mblabs.com.br>
2023-01-25 22:23:56 -0300

    First Commit

---
+++ main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "ordenacao.h"
+
+void imprimeVetor(int vetor[], int tam)
+{
+	printf("Vetor:");
+	for (int i = 0; i < tam; i++)
+	{
+		printf("%d ", vetor[i]);
+	}
+}
+
+int *criaVetor(int vetor[], int tam)
+{
+	int i;
+	srand(time(NULL));
+	for (i = 0; i < tam; i++)
+	{
+		vetor[i] = rand() % tam + 1;
+	}
+	return vetor;
+}
+
+int main()
+{
+	char nome[MAX_CHAR_NOME];
+	int idxBusca;
+	int numComp;
+	// Para medir o tempo, inclua time.h, e siga o exemplo:
+	clock_t start, end; // variáveis do tipo clock_t
+	double total;
+
+	int tamVetor = 100000;
+	int *vetor = malloc(tamVetor * sizeof(int));
+	if (vetor == NULL)
+	{
+		printf("Falha fatal. Impossível alocar memoria.");
+		return 1;
+	}
+
+	getNome(nome);
+	printf("Trabalho de %s\n", nome);
+	printf("GRR %u\n\n", getGRR());
+
+	printf("\n--------Vetor Desordenado--------\n");
+	criaVetor(vetor, tamVetor);
+	/* Busca Sequencial Test */
+	numComp = 0;
+	idxBusca = buscaSequencial(vetor, tamVetor, 1, &numComp);
+	printf("\nBusca Sequencial: \nindice do vetor = %d \nnumero de comparacoes = %d \n", idxBusca, numComp);
+
+	/* Busca Binária Test */
+	numComp = 0;
+	idxBusca = buscaBinaria(vetor, tamVetor, 16, &numComp);
+	printf("\nBusca binaria: \nindice do vetor = %d \nnumero de comparacoes = %d \n", idxBusca, numComp);
+
+	/* Insertion Sort Test */
+	criaVetor(vetor, tamVetor);
+	start = clock();
+	numComp = insertionSort(vetor, tamVetor);
+	end = clock();
+	total = ((double)end - start) / CLOCKS_PER_SEC;
+	printf("\nTempo total Insertion Sort: %f \n", total);
+	printf("\n");
+
+	/* Selection Sort Test */
+	criaVetor(vetor, tamVetor);
+	start = clock();
+	numComp = selectionSort(vetor, tamVetor);
+	end = clock();
+	total = ((double)end - start) / CLOCKS_PER_SEC;
+	printf("\nTempo total Selection Sort: %f \n", total);
+	printf("\n");
+
+	/* Merge Sort Test */
+	criaVetor(vetor, tamVetor);
+	start = clock();
+	numComp = mergeSort(vetor, tamVetor);
+	end = clock();
+	total = ((double)end - start) / CLOCKS_PER_SEC;
+	printf("\n");
+	printf("\nTempo total Merge Sort: %f \n", total);
+
+	/* Quick Sort Test */
+	criaVetor(vetor, tamVetor);
+	start = clock();
+	numComp = quickSort(vetor, tamVetor);
+	end = clock();
+	total = ((double)end - start) / CLOCKS_PER_SEC;
+	printf("\n");
+	printf("\nTempo total Quick Sort: %f \n", total);
+
+	/* Heap Sort Test */
+	/* criaVetor(vetor, tamVetor);
+	imprimeVetor(vetor, 10);
+	start = clock();
+	numComp = heapSort(vetor, tamVetor);
+	end = clock();
+	total = ((double)end - start) / CLOCKS_PER_SEC;
+	printf("\n");
+	imprimeVetor(vetor, 10);
+	printf("\nTempo total Heap Sort: %f \n", total); */
+
+	printf("\n-----------Vetor Ordenado---------\n");
+	/* Busca Sequencial Test */
+	numComp = 0;
+	idxBusca = buscaSequencial(vetor, tamVetor, 16, &numComp);
+	printf("\nBusca Sequencial: \nindice do vetor = %d \nnumero de comparacoes = %d \n", idxBusca, numComp);
+	/* Busca Binária Test */
+	numComp = 0;
+	idxBusca = buscaBinaria(vetor, tamVetor, 16, &numComp);
+	printf("\nBusca binaria: \nindice do vetor = %d \nnumero de comparacoes = %d \n", idxBusca, numComp);
+
+	free(vetor);
+
+	return 0;
+}
---
+++ ordenacao.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ordenacao.h"
+
+void getNome(char nome[])
+{
+	// substitua por seu nome
+	strncpy(nome, "Mateus Siqueira Ruzene", MAX_CHAR_NOME);
+	nome[MAX_CHAR_NOME - 1] = '\0'; // adicionada terminação manual para caso de overflow
+}
+
+// a função a seguir deve retornar o seu número de GRR
+unsigned int getGRR()
+{
+	return 20221223;
+}
+
+/* Funções Auxiliares */
+// insere elementos no vetor
+void insereVetor(int arr[], int tam)
+{
+	int key = arr[tam];
+	int j = tam - 1;
+	while (j >= 0 && arr[j] > key)
+	{
+		arr[j + 1] = arr[j];
+		j = j - 1;
+	}
+	arr[j + 1] = key;
+}
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void merge(int vetor[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	int L[n1], R[n2];
+
+	for (i = 0; i < n1; i++)
+	{
+		L[i] = vetor[l + i];
+	}
+	for (j = 0; j < n2; j++)
+	{
+		R[j] = vetor[m + 1 + j];
+	}
+
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			vetor[k] = L[i];
+			i++;
+		}
+		else
+		{
+			vetor[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1)
+	{
+		vetor[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2)
+	{
+		vetor[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+	int temp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = temp;
+	return (i + 1);
+}
+
+/* Funções Recursivas */
+int buscaBinariaRecursiva(int vetor[], int esquerda, int direita, int valor, int *numComparacoes)
+{
+	if (esquerda > direita)
+		return -1;
+
+	int meio = esquerda + (direita - esquerda) / 2;
+
+	(*numComparacoes)++;
+	if (vetor[meio] == valor)
+		return meio;
+	else if (vetor[meio] > valor)
+	{
+		(*numComparacoes)++;
+		return buscaBinariaRecursiva(vetor, esquerda, meio - 1, valor, numComparacoes);
+	}
+	else
+	{
+		(*numComparacoes)++;
+		return buscaBinariaRecursiva(vetor, meio + 1, direita, valor, numComparacoes);
+	}
+}
+
+int selectionSortRecursive(int vetor[], int n, int index)
+{
+	if (index == n)
+		return -1;
+
+	// acha o menor valor do vetor
+	int min_index = index;
+	for (int i = index + 1; i < n; i++)
+	{
+		if (vetor[i] < vetor[min_index])
+		{
+			min_index = i;
+		}
+	}
+	swap(&vetor[min_index], &vetor[index]);
+	return selectionSortRecursive(vetor, n, index + 1);
+}
+
+int mergeSortRecursive(int vetor[], int l, int r)
+{
+	if (l >= r)
+		return -1;
+	int m = l + (r - l) / 2;
+
+	mergeSortRecursive(vetor, l, m);
+	mergeSortRecursive(vetor, m + 1, r);
+
+	merge(vetor, l, m, r);
+
+	return 0;
+}
+
+int quickSortRecursive(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		quickSortRecursive(arr, low, pi - 1);
+		quickSortRecursive(arr, pi + 1, high);
+	}
+	return -1;
+}
+
+/* Funções principais */
+int buscaSequencial(int vetor[], int tam, int valor, int *numComparacoes)
+{
+	(*numComparacoes)++;
+	if (tam == 0)
+		return -1;
+	if (vetor[tam - 1] == valor)
+	{
+		(*numComparacoes)++;
+		return tam - 1;
+	}
+	return buscaSequencial(vetor, tam - 1, valor, numComparacoes);
+}
+
+int buscaBinaria(int vetor[], int tam, int valor, int *numComparacoes)
+{
+	return buscaBinariaRecursiva(vetor, 0, tam, valor, numComparacoes);
+}
+
+int insertionSort(int vetor[], int tam)
+{
+	if (0 >= tam)
+	{
+		return 1;
+	}
+	insertionSort(vetor, tam - 1);
+	insereVetor(vetor, tam - 1);
+	return 1;
+}
+
+int selectionSort(int vetor[], int tam)
+{
+	return selectionSortRecursive(vetor, tam, 0);
+}
+
+int mergeSort(int vetor[], int tam)
+{
+	return mergeSortRecursive(vetor, 0, tam);
+}
+
+int quickSort(int vetor[], int tam)
+{
+	return quickSortRecursive(vetor, 0, tam);
+}
+/*
+int heapSort(int vetor[], int tam)
+{
+	vetor[0] = 99;
+	return -1;
+} */
